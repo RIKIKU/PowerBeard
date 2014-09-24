4fd792c390f1145c41e354f1f21b103bfa9302ce
Kyle Smith <sniperwolf3@gmail.com>
2014-09-24 19:01:13 +1000

    Started creation of two new functions.  one function allows the user to store server connection info in a variable and the other uses the server connection info to retrieve the TVDBID based on the show's name. They are both not finished yet.

---
+++ Get-SickApiTvDbID.ps1
@@ -0,0 +1,18 @@
+﻿Function Get-SickApiTvdbID{
+Param ($ShowName)
+
+#server info needs to come in here
+
+
+
+$url = $urlbase + "/?cmd=" + $ApiCMD
+
+[net.httpWebRequest] $request  = [net.webRequest]::create($url)
+[net.httpWebResponse] $response = $request.getResponse()
+$responseStream = $response.getResponseStream()
+$sr = new-object IO.StreamReader($responseStream)
+$result = $sr.ReadToEnd()
+ConvertFrom-Json $result
+
+
+}
---
+++ New-SickApiConnection.ps1
@@ -0,0 +1,17 @@
+﻿Function New-SickAPIConnection {
+Param ($Server, $Port, $ApiKey, $ssl = $false)
+
+if (($ssl -eq $true) -or ($ssl -match 'yes|Yes|y|Y')) {
+    $urlbase = "https://$server`:$Port/api/$ApiKey/"
+    # ignore certificate errors
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+    [System.Net.ServicePointManager]::Expect100Continue = {$true}
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::ssl3
+    }
+else {
+    $urlbase = "http://$server`:$Port/api/$ApiKey/"
+    }
+#need to output the $urlbase and be able to pipe it into another function.
+$urlbase 
+
+}
