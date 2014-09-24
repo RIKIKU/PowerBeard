8b0eee3e0f138854c3dba1876e8496cef8ea9eb3
Kyle Smith <sniperwolf3@gmail.com>
2014-09-24 14:52:20 +1000

    Added first basic function which allows a user to use any API command. overall design of module not yet defined.

---
+++ New-SickAPIQuery.ps1
@@ -0,0 +1,32 @@
+﻿<#  Example of usage:
+(SickAPI -Server localhost -Port 8081 -ApiKey ab3b1539af60c8d65775081a9fa1485f -ssl $false -ApiCMD "sb.searchtvdb&name=south park").data
+if an APICMD is not specified the default of "sb" is used which will return general SickBeard server info.
+
+Credit where due:
+Github user Biranaddicks is the original creator of most of the code herein. I have simply taken part of it and turned it into
+a function so it can be used to query a server more easily. His original content can be found here: 
+https://github.com/brianaddicks/PowerShell/blob/master/Sickbeard-SnatchedToWanted.ps1
+#>
+
+Function New-SickAPIQuery {
+Param ($Server, $Port, $ApiKey, $ssl, $ApiCMD = "sb")
+
+if ($ssl) {
+    $urlbase = "https://$server`:$Port/api/$ApiKey/"
+    # ignore certificate errors
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+    [System.Net.ServicePointManager]::Expect100Continue = {$true}
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::ssl3
+} else {
+    $urlbase = "http://$server`:$Port/api/$ApiKey/"
+}
+
+$url = $urlbase + "/?cmd=" + $ApiCMD
+
+[net.httpWebRequest] $request  = [net.webRequest]::create($url)
+[net.httpWebResponse] $response = $request.getResponse()
+$responseStream = $response.getResponseStream()
+$sr = new-object IO.StreamReader($responseStream)
+$result = $sr.ReadToEnd()
+ConvertFrom-Json $result
+}
