b557acd74d205875fb993e95911a25b8fd7aa4f4
Kyle Smith <sniperwolf3@gmail.com>
2014-09-27 23:21:56 +1000

    added new funciton  that uses the "show" api command. seems to bring back the data in a challenging way. will need to figure out how to dynamically deal with changing headings.

---
+++ Get-PowerBeardShows.ps1
@@ -0,0 +1,31 @@
+﻿Function Get-PowerBeardShows{
+    [CmdletBinding()]
+    Param (
+            [Parameter(Mandatory=$True,ValueFromPipelinebyPropertyName=$True)][string[]]$ServerConnectionString
+            )
+
+    Begin{
+          }
+    Process{
+        #compile the appropriate URL here.
+            [string]$url = $ServerConnectionString + "?cmd=shows"
+            
+        #send request to server and retrieve output
+            [net.httpWebRequest] $request  = [net.webRequest]::create($url)
+            [net.httpWebResponse] $response = $request.getResponse()
+            $responseStream = $response.getResponseStream()
+            $sr = new-object IO.StreamReader($responseStream)
+            $result = $sr.ReadToEnd()
+            $PreprocessInfo = ConvertFrom-Json $result
+            Write-Output $PreprocessInfo.data.78825
+           <# if($PreprocessInfo.result -eq "success"){
+                $showinfo = $PreprocessInfo.data
+                Add-Member -InputObject $showinfo NoteProperty tvdbid $tvdbid -PassThru | Add-Member NoteProperty result success -PassThru |Add-Member NoteProperty tvdbid
+                }
+            else{
+                Add-Member -InputObject $PreprocessInfo NoteProperty tvdbid $tvdbid -PassThru | select -Property message, result, tvdbid
+                }#>
+        }
+    End{
+        }
+}
