76d3fadf3540747af28391214b80c5a0f1eb6799
Kyle Smith <sniperwolf3@gmail.com>
2014-09-28 22:23:27 +1000

    having a lot of trouble iterating though the property names so that i can display the output of each property separatly.

--- Get-PowerBeardShows.ps1
+++ Get-PowerBeardShows.ps1
@@ -17,15 +17,22 @@
             $sr = new-object IO.StreamReader($responseStream)
             $result = $sr.ReadToEnd()
             $PreprocessInfo = ConvertFrom-Json $result
-            Write-Output $PreprocessInfo.data.78825
-           <# if($PreprocessInfo.result -eq "success"){
-                $showinfo = $PreprocessInfo.data
-                Add-Member -InputObject $showinfo NoteProperty tvdbid $tvdbid -PassThru | Add-Member NoteProperty result success -PassThru |Add-Member NoteProperty tvdbid
+            
+            if($PreprocessInfo.result -eq "success"){
+                [array]$shows = $PreprocessInfo.data
+                $tvdbids = ($shows | Get-Member | Where-Object -Property Name -NotMatch 'a|e|i|o|u' | select -Property Name)
+                foreach ($tvdbid in $tvdbids){
+                         $output = ($PreprocessInfo.data."$tvdbid.name")
+                         Write-Output $output
+                        }
                 }
+                #Add-Member -InputObject $showinfo NoteProperty tvdbid $tvdbid -PassThru | Add-Member NoteProperty result success -PassThru |Add-Member NoteProperty tvdbid
             else{
-                Add-Member -InputObject $PreprocessInfo NoteProperty tvdbid $tvdbid -PassThru | select -Property message, result, tvdbid
-                }#>
-        }
+                write-host "else occurred"
+                #Add-Member -InputObject $PreprocessInfo NoteProperty tvdbid $tvdbid -PassThru | select -Property message, result, tvdbid
+                }
+            }
+        
     End{
         }
 }
