5e3f73e0b60375137007b814b7a157124e17f18e
Kyle Smith <sniperwolf3@gmail.com>
2014-11-18 09:25:18 +1100

    added Stop-Service

--- PowerBeard/PowerBeard.psm1
+++ PowerBeard/PowerBeard.psm1
@@ -1093,33 +1093,9 @@ Function Restart-Service{
     .EXAMPLE
         New-PowerBeardConnection -Server MySickBeardServer -Port 8081 -ApiKey ab31537af30c8d65765081a9fa148ff | Restart-PowerBeardService
 
-data                                           message                                       result                                       
-----                                           -------                                       ------                                       
-                                               Breaking Bad has been deleted                 success
-
-In this example; the show "Breaking Bad" was deleted.
-
-    .EXAMPLE
-        $ServerConnectionString = (New-PowerBeardConnection -Server localhost -Port 8081 -ApiKey 8ba833c4eddf362f567c4f64b637402e)
-        $ServerConnectionString | Get-PowerBeardShows | export-csv C:\Users\kyles_000\Documents\PBTest.csv
-
-
-$test = import-csv C:\Users\kyles_000\Documents\PBTest.csv
-foreach($tvdbid in $test.tvdbid){ 
-$ServerConnectionString | Remove-PowerBeardShow $tvdbid}
-
-message                                       result                                       
--------                                       ------                                       
-Archer (2009) has been deleted                success                                      
-Game of Thrones has been deleted              success                                      
-Spartacus has been deleted                    success                                      
-Blue Mountain State has been deleted          success
-
-In this example, a csv file was created that contained all of the shows that I currently have.(ran seperatly)
-I then removed the shows that I didnt want to delete, from the CSV file and imported it into a variable, where I created
-a loop to delete each show in the list.
-
-
+data                 message                          result                                       
+----                 -------                          ------                                       
+                     SickBeard is restarting...       success
     #>
     [CmdletBinding()]
     Param (
@@ -1133,6 +1109,48 @@ a loop to delete each show in the list.
     Process{
         #compile the appropriate URL here.
         [string]$APICMD = "sb.restart"
+        $PreprocessInfo = $ServerConnectionString | New-PowerBeardCommand -ApiCMD $APICMD
+        Write-Output $PreprocessInfo
+        }
+    End{
+        Get-Variable | 
+        where {$sysvars -notcontains $_.Name} |
+        foreach {Remove-Variable $_ -ErrorAction SilentlyContinue}
+        }
+}
+
+Function Stop-Service{
+    <#
+    .SYNOPSIS
+        Used to shutdown SickBeard.
+
+        
+    .DESCRIPTION
+        This funciton restarts the sickbeard server.  
+
+    .PARAMETER  ServerConnectionString
+        This parameter accepts pipeline input from New-PowerBeardConnection. A correctly formated URI or variable may
+        be used here instead.
+
+    .EXAMPLE
+        New-PowerBeardConnection -Server MySickBeardServer -Port 8081 -ApiKey ab31537af30c8d65765081a9fa148ff | Restart-PowerBeardService
+
+data                                           message                                       result                                       
+----                                           -------                                       ------                                       
+                                               SickBeard is shutting down...                 success 
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,ValueFromPipelinebyPropertyName=$True)][string[]]$ServerConnectionString
+            )
+    Begin{
+        $sysvars = Get-Variable |
+        select -ExpandProperty Name
+        $sysvars += 'sysvars'
+          }
+    Process{
+        #compile the appropriate URL here.
+        [string]$APICMD = "sb.shutdown"
         $PreprocessInfo = $ServerConnectionString | New-PowerBeardCommand -ApiCMD $APICMD
         Write-Output $PreprocessInfo
         }
